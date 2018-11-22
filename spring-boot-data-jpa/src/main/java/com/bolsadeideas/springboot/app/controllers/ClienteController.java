77e6444daea305ed4317af30e144d05f4a666211
borjiales <borjiales@gmail.com>
2018-11-22 16:57:00 +0100

    1º Commit , cambio de equipo.

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -0,0 +1,47 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+@Controller
+public class ClienteController {
+	
+	@Autowired
+	@Qualifier("clienteDaoJPA")
+	private IClienteDao clienteDao;
+	
+	@RequestMapping(value="/listar", method=RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo","listado de clientes");
+		model.addAttribute("clientes", clienteDao.findAll());
+		return "listar";
+	}
+	
+	
+	@RequestMapping(value="/form")
+	public String crear(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("titulo", "formulario de Cliente");
+		return "form";
+	}
+	
+	@RequestMapping(value="/form", method=RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente, BindingResult result) {
+		if (result.hasErrors()) {
+			return "form";
+		}
+		clienteDao.save(cliente);
+		return "redirect:listar";
+	}
+}
